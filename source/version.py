ef6c6e5fc6243cd55cd51eb0dc1c5054c45f0e08
scapella <salcagu@gmail.com>
2016-09-11 23:24:25 +0200

    Initial commit for a new auxiliary script. This script will take as input a file containing all sequences related to a given analysis, and create an appropriate structure to store all intermediate files. It will also generate the call to be execute - one per input file - in a high performance computing facility.

--- source/version.py
+++ source/version.py
@@ -19,6 +19,6 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__build__ = "2016-05-23"
-__revision__ = "13"
+__build__ = "2016-05-28"
+__revision__ = "14"
 __version__ = "1.0.1"
